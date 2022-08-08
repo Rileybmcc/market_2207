225272816af87a1025218a234abd8afa4b4c5d8b
Riley McCullough <rileybmccullough@gmail.com>
2022-08-08 09:13:03 -0600

    Initial commit

---
+++ spec/item_spec.rb
@@ -0,0 +1,22 @@
+require './lib/item'
+
+RSpec.describe Item do
+
+  before :each do
+    @item1 = Item.new({name: 'Peach', price: "$0.75"})
+    @item2 = Item.new({name: 'Tomato', price: '$0.50'})
+  end
+
+  it 'can instantiate item' do
+    expect(@item1).to be_instance_of(Item)
+  end
+
+  xit 'can access item name' do
+    expect(@item2.name).to eq("Tomato")
+  end
+
+ xit 'can access item price' do
+   expect(@item2.price).to eq(0.5)
+ end
+
+end
